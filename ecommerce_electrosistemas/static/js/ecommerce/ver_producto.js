f5a575cc7853fb64e193ed6f190a355071d71e75
Pablo <cpablop1@gmail.com>
2024-10-10 19:17:07 -0600

    Ver producto en ecommerce terminado

--- ecommerce_electrosistemas/static/js/ecommerce/ver_producto.js
+++ ecommerce_electrosistemas/static/js/ecommerce/ver_producto.js
@@ -3,10 +3,33 @@ export function ver_producto(data) {
 
     fetch(`/producto/listar-productos?${params.toString()}`).then(data => data.json()).then(data => {
         console.log(data.data[0]);
+        let img = '';
+        let indicador = `<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="bg-primary active" aria-current="true" aria-label="Slide 1"></button>`;
         document.getElementById('descripcion_producto').innerHTML = data.data[0].descripcion;
         document.getElementById('precio_producto').innerHTML = `Q. ${data.data[0].precio_publico}`;
         document.getElementById('stock_producto').innerHTML = `Existencias: ${data.data[0].stock}`;
         document.getElementById('marca_producto').innerHTML = `Marca: ${data.data[0].marca}`;
+
+        if (data.data[0].img_1.trim().length != 0) {
+            img += `<div class="carousel-item active">
+                        <img src="./../media/${data.data[0].img_1}" class="d-block mx-auto" alt="..." style="width: 400px; height: auto;">
+                    </div>`;
+        } else {
+            img += `<div class="carousel-item active">
+                        <img src="./../static/img/not_found.png" class="d-block mx-auto" alt="..." style="width: 500px; height: auto;">
+                    </div>`;
+        }
+
+        if (data.data[0].img_2.trim().length != 0) {
+            img += `<div class="carousel-item">
+                        <img src="./../media/${data.data[0].img_2}" class="d-block mx-auto" alt="..." style="width: 400px; height: auto;">
+                    </div>`;
+            indicador += `<button class="bg-primary" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>`;
+        }
+
+        document.getElementById('indicador_producto').innerHTML = indicador;
+
+        document.getElementById('img_producto').innerHTML = img;
         new bootstrap.Modal(document.getElementById('modal_ver_producto')).show(); // Instrucción para mostrar la modal
 
     });
